56245a64abe618fcc615f1921856c9d53a1f721e
BallsyBread <44901003+BallsyBread@users.noreply.github.com>
2024-03-18 23:22:05 +0100

    create intro scene and main menu

---
+++ 3d/3d_realms/Scenes/MainMenu/MainMenu.gd
@@ -0,0 +1,10 @@
+extends VBoxContainer
+
+func _on_single_player_pressed():
+	pass # Replace with function body.
+
+func _on_multiplayer_pressed():
+	pass # Replace with function body.
+
+func _on_settings_pressed():
+	pass # Replace with function body.
